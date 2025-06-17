ed23214bf9ed1c3afd1d53ffe533bbf73fb23dce
Angrez04 <hariharanman@outlook.com>
2025-06-17 17:38:52 +0530

    don't know what I did

--- ProgramFiles/python/AI_handler.py
+++ ProgramFiles/python/AI_handler.py
@@ -24,15 +24,15 @@ to resolve or mitigate the problem.
 """
     # Establish Connection
     def __init__(self) -> None:
-        self.AZURE_OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
-        self.AZURE_DEPLOYMENT_NAME = os.getenv("AZURE_DEPLOYMENT_NAME")
-        self.AZURE_RESOURCE_NAME = os.getenv("AZURE_RESOURCE_NAME")
-        self.AZURE_API_VERSION = os.getenv("AZURE_API_VERSION")
+        AZURE_OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
+        AZURE_DEPLOYMENT_NAME = os.getenv("AZURE_DEPLOYMENT_NAME")
+        AZURE_RESOURCE_NAME = os.getenv("AZURE_RESOURCE_NAME")
+        AZURE_API_VERSION = os.getenv("AZURE_API_VERSION")
         self.chat = AzureChatOpenAI(
-                azure_deployment=self.AZURE_DEPLOYMENT_NAME,  # type: ignore
-                azure_endpoint=f"https://{self.AZURE_RESOURCE_NAME}.openai.azure.com/",
-                api_key=self.AZURE_OPENAI_API_KEY, # type: ignore
-                api_version=self.AZURE_API_VERSION,
+                azure_deployment=AZURE_DEPLOYMENT_NAME,  # type: ignore
+                azure_endpoint=f"https://{AZURE_RESOURCE_NAME}.openai.azure.com/",
+                api_key=AZURE_OPENAI_API_KEY, # type: ignore
+                api_version=AZURE_API_VERSION,
                 temperature=0.2
             )
         
