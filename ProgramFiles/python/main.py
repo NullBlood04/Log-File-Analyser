b37c63a5595f18cbeedf7c497d2eb26ced12c8bc
Angrez04 <hariharanman@outlook.com>
2025-06-17 11:28:28 +0530

    added seperate streamlit class and streamlining the flow

--- ProgramFiles/python/main.py
+++ ProgramFiles/python/main.py
@@ -1,31 +1,64 @@
-# Library imports
-from langchain.schema import SystemMessage, HumanMessage
-
 # Local imports
 from Log_to_Csv import EventlogtoCSV
 from represent_csv import LoadCSV
 from AI_handler import Connect_AI
 import streamlit as st
+from filter_unique_field import GenerateUniqueField
 
         
 class StreamlitRendering:
 
+
+    def __init__(self) -> None:
+        self.connect = Connect_AI()
+        self.load_csv = LoadCSV()
+
+
+    def button_click(self, dictionary, index):
+        prompt_string = f"""
+Event ID : {dictionary["EventID"][index]}
+Source : {dictionary["Source"][index]}
+Entry Type : {dictionary["EntryType"][index]}
+Message : {dictionary["Message"][index]}
+"""
+        result = self.connect.prompt(prompt_string)
+        st.markdown(result) # TODO: change this to stylise
+
+
     def streamlit_gui(self) -> None:
-        pass
+
+        self.load_csv.extract_from("CSVfiles\\UniqueErrors.csv")
+        st.set_page_config(page_title="Event Log Analyser", layout="wide")
+        st.title("Event Log Analyser")
+
+        st.dataframe(self.load_csv.display_csv())
+        csv_dict = self.load_csv.display_csv_dict()
+
+        timegendict = csv_dict["TimeGenerated"]
+        count = timegendict
+        button = []
+        for key in count:
+            button.append(st.button(label=csv_dict["Message"][key]))
+
+        for i, each_button in enumerate(button):
+            if each_button:
+                self.button_click(csv_dict, i)
+
 
 
 class Main:
+        
 
     def main(self) -> None:
-        #connect = Connect_AI()
-        load_csv = LoadCSV()
         convert_to_csv = EventlogtoCSV("Microsoft-Windows-Perflib")
         convert_to_csv.extract_evt_files()
-        load_csv.extract_from("CSVfiles\\UniqueErrors.csv")
-        st.set_page_config(page_title="Event Log Analyser", layout="wide")
-        st.title("Event Log Analyser")
-        st.dataframe(load_csv.display_csv())
+        unique_csv = GenerateUniqueField()
+        unique_csv.gen_unique()
 
+        gui = StreamlitRendering()
+        gui.streamlit_gui()
+
+        
 
 if __name__ == "__main__":
     main = Main()
