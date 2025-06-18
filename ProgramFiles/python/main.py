0eced18d1b712da6db3f081497fe83612249a1f7
Angrez04 <hariharanman@outlook.com>
2025-06-18 13:16:55 +0530

    Seperated streamlit class

--- ProgramFiles/python/main.py
+++ ProgramFiles/python/main.py
@@ -1,50 +1,9 @@
 # Local imports
 from Log_to_Csv import EventlogtoCSV
-from represent_csv import LoadCSV
-from AI_handler import Connect_AI
-import streamlit as st
 from filter_unique_field import GenerateUniqueField
+from StreamlitGUI import StreamlitRendering
 
         
-class StreamlitRendering:
-
-
-    def __init__(self) -> None:
-        self.connect = Connect_AI()
-        self.load_csv = LoadCSV()
-
-
-    def button_click(self, dictionary, index):
-        prompt_string = f"""
-Event ID : {dictionary["EventID"][index]}
-Source : {dictionary["Source"][index]}
-Entry Type : {dictionary["EntryType"][index]}
-Message : {dictionary["Message"][index]}
-"""
-        result = self.connect.prompt(prompt_string)
-        st.markdown(result) # TODO: change this to stylise
-
-
-    def streamlit_gui(self) -> None:
-
-        self.load_csv.extract_from("CSVfiles\\UniqueErrors.csv")
-        st.set_page_config(page_title="Event Log Analyser", layout="wide")
-        st.title("Event Log Analyser")
-
-        st.dataframe(self.load_csv.display_csv())
-        csv_dict = self.load_csv.display_csv_dict()
-
-        timegendict = csv_dict["TimeGenerated"]
-        count = timegendict
-        button = []
-        for key in count:
-            button.append(st.button(label=csv_dict["Message"][key]))
-
-        for i, each_button in enumerate(button):
-            if each_button:
-                self.button_click(csv_dict, i)
-
-
 
 class Main:
         
