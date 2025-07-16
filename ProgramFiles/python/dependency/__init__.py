bf03aac44147f044d5385ade9dae289c2a4e0738
Angrez04 <hariharanman@outlook.com>
2025-07-16 17:13:24 +0530

    add: commented the use of functions

--- ProgramFiles/python/dependency/__init__.py
+++ ProgramFiles/python/dependency/__init__.py
@@ -5,6 +5,8 @@ from .AdditionalTools.createDatabase import create_errorDbase
 
 __all__ = ["ChatBot"]
 
-
+# Automaticaly creates database and table if not exists
 create_errorDbase()
+
+# Inserts data into tables
 data_insert()
