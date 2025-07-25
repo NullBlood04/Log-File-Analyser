50b4322a5b74ddc2134bd66d18101c1beb30987a
Angrez04 <hariharanman@outlook.com>
2025-07-25 16:39:42 +0530

    add: second var - event_log

--- ProgramFiles/powershell/extract_logs.ps1
+++ ProgramFiles/powershell/extract_logs.ps1
@@ -1,8 +1,9 @@
 param(
-    $last_record_id
+    $last_record_id,
+    $log_name
 )
 
-Get-WinEvent -LogName Application | 
+Get-WinEvent -LogName $log_name | 
 Where-Object { $_.LevelDisplayName -in @('Error','Critical') -and $_.RecordId -gt $last_record_id } |
 Select-Object Id, LevelDisplayName, ProviderName, TimeCreated, Message, RecordId | 
 ConvertTo-Json
