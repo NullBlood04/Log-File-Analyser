29a20bb89de9c90f41946e23b9a0d3fe1fc2e4a7
Angrez04 <hariharanman@outlook.com>
2025-07-27 13:52:22 +0530

    add: returns in UTF-8 format

--- ProgramFiles/powershell/extract_logs.ps1
+++ ProgramFiles/powershell/extract_logs.ps1
@@ -3,6 +3,8 @@ param(
     $log_name
 )
 
+[System.Console]::OutputEncoding = [System.Text.UTF8Encoding]::new($false)
+
 Get-WinEvent -LogName $log_name | 
 Where-Object { $_.LevelDisplayName -in @('Error','Critical') -and $_.RecordId -gt $last_record_id } |
 Select-Object Id, LevelDisplayName, ProviderName, TimeCreated, Message, RecordId | 
